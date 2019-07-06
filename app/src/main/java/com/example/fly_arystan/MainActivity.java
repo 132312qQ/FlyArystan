e9d6e8e31d541eb12a100c35caa9e4f97b4d42d7
Nursultan Rzabekov <nursbrat991229@gmail.com>
2019-07-06 20:30:01 +0600

    Add fragment

--- app/src/main/java/com/example/fly_arystan/MainActivity.java
+++ app/src/main/java/com/example/fly_arystan/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.fly_arystan;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new BookFragment()).commit();
+        }
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_flight:
+                            selectedFragment = new BookFragment();
+                            break;
+                        case R.id.nav_registration:
+                            selectedFragment = new RegistrationFragment();
+                            break;
+                        case R.id.nav_status:
+                            selectedFragment = new StatusFragment();
+                            break;
+                        case R.id.nav_timetable:
+                            selectedFragment = new TimetableFragment();
+                            break;
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
